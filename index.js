2106dcfc604628b29b6907be02a0564ce8fcfab8
Frederick Manyo <manyofrederick99@gmail.com>
2024-09-27 00:39:14 +0000

    adding banners and others

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('Sunday-service-label').addEventListener('click', () => {
+    window.location.href = '#Sunday_service-img-container';
+})
+
+document.getElementById('Tuesday-service-label').addEventListener('click', () => {
+    window.location.href = '#Tuesday_service-img-container';
+})
+
+document.getElementById('Healing-service-label').addEventListener('click', () => {
+    window.location.href = '#Healing_service-img-container';
+})
