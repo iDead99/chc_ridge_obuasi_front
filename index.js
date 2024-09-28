acfd85e23d8e509dcd15444f93f16e338a79139f
Frederick Manyo <manyofrederick99@gmail.com>
2024-09-28 23:45:32 +0000

    adding event flyers

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ document.getElementById('Tuesday-service-label').addEventListener('click', () =>
 document.getElementById('Healing-service-label').addEventListener('click', () => {
     window.location.href = '#Healing_service-img-container';
 })
+
+// Get the header element
+const header = document.getElementById("header");
+
+// Get the offset position of the header
+const sticky = header.offsetTop;
+
+// Add or remove the "sticky" class when you scroll
+window.onscroll = function() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+};
